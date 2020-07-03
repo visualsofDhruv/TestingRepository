c7ae98dfc231510b46ae77e72d43466e443085b4
visualsofDhruv <dhruv.prajapati@crestdatasys.com>
2020-07-03 16:10:41 +0530

    updated

--- Calculator.py
+++ Calculator.py
@@ -1,33 +1,34 @@
-class Calc:
-    
-    @staticmethod
-    def sum(a, b):
-        c = a + b
-        return c
-    
-    @staticmethod
-    def sub(a, b):
-        c = a - b
-        return c
-    
-    @staticmethod
-    def mul(a, b):
-        c = a * b
-        return c
-    
-    @staticmethod
-    def div(a, b):
-        c = a * b
-        return c
+def sums(a, b):
+    c = a + b
+    return c
 
 
-class Main:
+def sub(a, b):
+    c = a - b
+    return c
+
+
+def mul(a, b):
+    c = a * b
+    return c
+
+
+def div(a, b):
+    c = a * b
+    return c
+
+
+if __name__ == '__main__':
     
     a = int(input('enter no. one: '))
     b = int(input('enter no. one: '))
 
-    calcObj = Calc()
-    calcObj.sum(a, b)
-    calsObj.sub(a, b)
-    calcObj.mul(a, b)
-    calsObj.div(a, b)
+    sumans = sums(a, b)
+    subans = sub(a, b)
+    mulans = mul(a, b)
+    divans = div(a, b)
+    
+    print('Sum:', sumans)
+    print('Sub:', subans)
+    print('Mul:', mulans)
+    print('Div:', divans)
